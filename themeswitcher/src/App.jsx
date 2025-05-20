94eddd776ea02491e2056544c05882e3c218ec2d
Muzamil Hussain <muzamilhussain369@gmail.com>
2025-05-20 21:28:01 +0500

    themeswticher first

---
+++ themeswitcher/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='p-4'>
+      <h1 className='p-4 text-center bg-pink-400 text-white'>Hey Muzamil Hussain</h1>
+    </div>
+  )
+}
+
+export default App
